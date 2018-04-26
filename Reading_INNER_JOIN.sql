33ab7626bf2b1eda9dcc19125bcc70ee03c0ca1c
Nivaan <krishunduttn@gmail.com>
2018-04-26 22:46:53 +0200

    Further cleanup

--- Reading_INNER_JOIN.sql
+++
@@ -1,5 +0,0 @@
-SELECT employee.*
-,(GROUP_CONCAT(`equipment`.`equip_id` ORDER BY `equipment`.`equip_id` ASC SEPARATOR ', ')) AS 'Equipment Loaned Out'
-FROM `emss_database`.`employee`
-LEFT JOIN `emss_database`.`equipment` ON `employee`.`employee_id` = `equipment`.`loaned_out_to`
-GROUP BY 1;
